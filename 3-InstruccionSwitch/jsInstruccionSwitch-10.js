3198a2317f1e671e863c5e37ceff6cdeb4e53c6c
MarianiAmira <124008835+MarianiAmira@users.noreply.github.com>
2023-02-10 18:44:53 -0300

    Add files via upload
    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,53 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada = txtIdEstacion.value;
+	var destino = txtIdDestino.value;
+	var mensaje;
 
+	switch(estacionIngresada){
+		case "Invierno":
+			switch(destino){
+				case "Bariloche":
+					mensaje = "se viaja";
+					break;
+				default:
+					mensaje = "no se viaja";
+			}
+		break
+		case "Verano":
+			switch(destino){
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje = "se viaja";
+					break;
+				default:
+					mensaje = "no se viaja";
+					break;
+			}
+		break
+		case "Otoño":
+			switch(destino){
+				case "Mar del plata":
+				case "bariloche":
+				case "Cordoba":
+				case "Cataratas":
+					mensaje = "se viaja";
+					break;
+				default:
+					mensaje = "no se viaja";
+					break;
+			}
+		break
+		default:
+			switch(destino){
+				case "Bariloche":
+					mensaje = "se viaja";
+					break;
+				default:
+					mensaje = "no se viaja";
+					break;
+			}
+	}
+	
+alert(mensaje);
 }//FIN DE LA FUNCIÓN
