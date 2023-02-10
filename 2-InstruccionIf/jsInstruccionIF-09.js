780b3700b9c51a143e0a0e864beebb95e672e707
MarianiAmira <124008835+MarianiAmira@users.noreply.github.com>
2023-02-10 18:17:25 -0300

    Add files via upload
    IF

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,5 @@
 function mostrar()
 {
-	//alert(Math.random() * 10 + 1);	
 	alert(Math.random(0,10));
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,5 +1,22 @@
 function mostrar()
 {
-	alert(Math.random(0,10));
-	
+	let nota;
+    nota = Math.random()*10 + 1;
+	nota = parseInt(nota);
+
+	alert(nota);
+
+    if (nota >= 9 || nota == 10)
+	{
+		alert("EXCELENTE");
+	}
+    else if (nota >= 4)
+	{
+		alert("APROBO");
+	}
+    else
+	{
+		alert("Vamos, la próxima se puede");
+	}
+
 }//FIN DE LA FUNCIÓN
